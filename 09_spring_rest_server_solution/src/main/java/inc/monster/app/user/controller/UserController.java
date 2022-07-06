4b6be6d76c5ee2a47d1402dc91c4959f4bb704be
Jonas Dittrich <jonas.dittrich@codecentric.de>
2022-07-06 15:39:31 +0200

    SOLUTION-COMPLETE: 09_spring_rest_server_solution

--- 09_spring_rest_server_solution/src/main/java/inc/monster/app/user/controller/UserController.java
+++ 09_spring_rest_server_solution/src/main/java/inc/monster/app/user/controller/UserController.java
@@ -1,10 +1,34 @@
 package inc.monster.app.user.controller;
 
+import inc.monster.app.user.domain.User;
+import inc.monster.app.user.service.UserService;
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return userService.getUser(id)
+                .map(user -> new ResponseEntity<>(user, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 
 }
--- 09_spring_rest_server_solution/src/test/java/inc/monster/app/hello/controller/HelloControllerTest.java
+++ 09_spring_rest_server_solution/src/test/java/inc/monster/app/hello/controller/HelloControllerTest.java
@@ -1,6 +1,5 @@
-package inc.monster.app.hello.resource;
+package inc.monster.app.hello.controller;
 
-import inc.monster.app.hello.controller.HelloController;
 import java.util.Collections;
 import java.util.Map;
 import org.hamcrest.Matchers;
@@ -8,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -16,12 +16,15 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @WebMvcTest(HelloController.class)
 public class HelloControllerTest {
 
+    private static final Map<String, String> XML_NS =
+            Collections.singletonMap("ns", "http://monster.inc/hello");
+
     @Autowired
     private MockMvc mvc;
 
     @Test
     public void getHello_Accept_JSON() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+        this.mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
@@ -30,15 +33,12 @@ public class HelloControllerTest {
 
     @Test
     public void getHello_Accept_XML() throws Exception {
-        Map<String, String> NS =
-                Collections.singletonMap("ns", "http://monster.inc/hello");
-
-        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_XML))
+        this.mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_XML))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_XML))
                 .andExpect(MockMvcResultMatchers.xpath(
-                        "/ns:hello/message", NS)
+                        "/ns:hello/message", XML_NS)
                         .string("Hello World!"));
     }
 }
