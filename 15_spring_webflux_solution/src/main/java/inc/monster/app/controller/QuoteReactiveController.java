75bafa2968efc9c11ef73adddfa979cc82fab739
Jonas Dittrich <jonas.dittrich@codecentric.de>
2022-07-06 15:39:32 +0200

    SOLUTION-BASE: 15_spring_webflux_solution

---
+++ 15_spring_webflux_solution/src/main/java/inc/monster/app/controller/QuoteReactiveController.java
@@ -0,0 +1,8 @@
+package inc.monster.app.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuoteReactiveController {
+
+}
---
+++ 15_spring_webflux_solution/src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+create table quote
+(
+    id      bigint,
+    book    varchar(255) not null,
+    content varchar(255) not null
+);
